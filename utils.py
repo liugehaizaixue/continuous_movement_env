e4708f238f125415dc09167b308d395954968fb9
lyl <845602274@qq.com>
2024-05-06 13:44:23 +0800

    add generate points

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
-
+from copy import deepcopy
 # 定义函数来生成一个不与障碍物或边界相交的随机出生点
 def generate_random_spawn_point(ball_radius, obstacles, WINDOW_WIDTH, WINDOW_HEIGHT , seed = 0):
     random.seed(seed)
@@ -31,8 +31,80 @@ def generate_random_spawn_point(ball_radius, obstacles, WINDOW_WIDTH, WINDOW_HEI
             return [random_x, random_y]
 
 
-# 生成随机的速度，并确保模长为1
+def random_point(matrix, r):
+    """ 随机选择点 , 保证选择的点的r范围内不是边界 也不是 障碍1
+    matrix 为矩阵
+    r 为范围
+    """
+    height = len(matrix)
+    width = len(matrix[0])
+
+    attempts = height * width  # 最多尝试次数
+
+    while attempts > 0:
+        x = random.randint(r, width - r - 1)
+        y = random.randint(r, height - r - 1)
+
+        valid = True
+        for i in range(y - r, y + r + 1):
+            for j in range(x - r, x + r + 1):
+                if i < 0 or i >= height or j < 0 or j >= width or matrix[i][j] == 1:
+                    valid = False
+                    break
+            if not valid:
+                break
+
+        if valid:
+            return (x, y)
+
+        attempts -= 1
+
+    return None
+
+def distance(p1, p2):
+    return ((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) ** 0.5
+
+def generate_points(matrix , r , k):
+    """ 
+        在matrix上
+        生成k对 start与tartget
+        其中starts之间满足距离大于2r，且start的r范围内没有障碍或边界外
+    """
+    matrix = deepcopy(matrix)
+    height = len(matrix)
+    width = len(matrix[0])
+    start_points = []
+    target_points = []
+    attempts = height * width
+    while len(start_points) < k and attempts > 0:
+        attempts -= 1
+        s_valid = True    
+        start = random_point(matrix , r)
+        if start:
+            for point in start_points:
+                if distance(start, point) < 2 * r:
+                    s_valid = False
+                    break
+            if s_valid: # 成功找到新的start
+                start_points.append(start)
+
+    attempts = height * width
+    while len(target_points) < k and attempts > 0:
+        attempts -= 1
+        target = random_point(matrix , r)
+        if target:
+            if target not in target_points:
+                target_points.append(target)
+
+
+    if len(start_points) == k and len(target_points) == k :
+        return start_points , target_points
+    else:
+        return None , None
+
+
 def generate_random_velocity():
+    """ 生成随机的速度，并确保模长为1 """
     angle = random.uniform(0, 2 * math.pi)  # 随机选择速度的方向
     velocity_x = math.cos(angle)
     velocity_y = math.sin(angle)
--- visualization.py
+++ visualization.py
@@ -1,11 +1,16 @@
 from custom_maps import MAPS_REGISTRY
-from utils import get_map_array , expand_matrix , visual_map
+from utils import *
 
 map_str = MAPS_REGISTRY["sc1-AcrosstheCape"]
 
 _map = get_map_array(map_str)
 
-_map = expand_matrix(_map , factor= 5)
+_map = expand_matrix(_map , factor= 10)
 
-visual_map(_map)
+# visual_map(_map)
+
+# point = random_point(_map , 5)
+starts , tartgets = generate_points(_map,5,128)
+
+print(starts , tartgets)
 
